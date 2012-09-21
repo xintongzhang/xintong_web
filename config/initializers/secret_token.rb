c09c24bf0a16f178fd60de48146fdd2e74ff686c
xintongzhang <annabelle.cmu@gmail.com>
2012-09-21 07:35:13 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+XintongWeb::Application.config.secret_token = '546412da7564ce49c47a294afc9e45969e97d9b14241caf3ce83dfea82776775626feb44e05a65029cfd40313b16a5688f1df3c8956f5fc0716d84c46dc7d0a7'
